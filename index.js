0b5ee2930d59eb2d4283fa9131ca781b3a81708a
Adrian <adrian.losicki@gmail.com>
2025-05-31 22:50:20 +0200

    final resolution

--- index.js
+++ index.js
@@ -5,54 +5,59 @@ let teach = document.querySelector("#teachers");
 
 let cont = document.querySelector(".cointainer3");
 
-let text = document.querySelector("#list");
-let text2 = document.querySelector("#list2");
+let text = document.querySelector(".subjects");
+let text2 = document.querySelector(".subjects2");
+
 
 let button1 = document.querySelector("#b");
 let button2 = document.querySelector("#b2");
 
-let box1 = document.querySelector(".box")
-let box2 = document.querySelector(".box2")
+let box1 = document.querySelector(".box");
+let box2 = document.querySelector(".box2");
 
 
 function remove(){
-    text.classList.remove("hidden")
+   
   
     if(sub.classList.contains("hidden")){
         sub.classList.remove("hidden");
+        text.classList.remove("hidden");
        
     } else
     {
         sub.classList.add("hidden");
+        text.classList.add("hidden");
     }
     
 };
 
 function remove2(){
-    text.classList.remove("hidden")
+    
     if(teach.classList.contains("hidden"))
-    {
+    {   
+        text2.classList.remove("hidden");
         teach.classList.remove("hidden");
     } else{
         teach.classList.add("hidden");
+        text2.classList.add("hidden");
     }
     
 };
 
 
 button1.addEventListener("mouseenter", () => {
-    box1.style.height = 1100 + "px"
+    box1.style.height = 1100 + "px";
 
 
     button1.addEventListener("mouseleave", () => {
-        box1.style.height = 835 + "px"
-    })
-})
+        box1.style.height = 835 + "px";
+    });
+});
 
 button2.addEventListener("mouseenter", () => {
-    box2.style.height = 1100 + "px"
+    box2.style.height = 1100 + "px";
 
     button2.addEventListener("mouseleave", () => {
-        box2.style.height = 835 + "px"
-    })
-})
+        box2.style.height = 835 + "px";
+    });
+});
